b597bd05e5d46d12548f6f8d96f19809360a602c
Arsalan Ansari <arsalanansariofficial@outlook.com>
2025-06-24 03:41:42 +0530

    Add header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import '@/app/globals.css';
 
 import { auth } from '@/auth';
 import Session from '@/app/session';
+import Header from '@/components/header';
 import { Toaster } from '@/components/ui/sonner';
 
 type Props = Readonly<{ children: React.ReactNode }>;
@@ -19,8 +20,9 @@ export default async function RootLayout({ children }: Props) {
   return (
     <html lang="en">
       <body className="grid min-h-screen grid-rows-[auto_1fr_auto] antialiased">
-        <Toaster />
+        <Header />
         <Session expiresAt={session?.user?.expiresAt}>{children}</Session>
+        <Toaster />
       </body>
     </html>
   );

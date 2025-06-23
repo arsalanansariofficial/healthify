ebe67b46e1fd0864f8b5ce4294c1a9db06b2078f
Arsalan Ansari <arsalanansariofficial@outlook.com>
2025-06-23 19:22:44 +0530

    Update create password page

--- app/create-password/component.tsx
+++ app/create-password/component.tsx
@@ -1,25 +1,73 @@
 'use client';
 
+import { toast } from 'sonner';
 import { useActionState } from 'react';
 
+import * as CN from '@/components/ui/card';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 import { updatePassword } from '@/lib/actions';
 import { Button } from '@/components/ui/button';
 
 export default function Component({ email }: { email: string }) {
-  const [state, action, pending] = useActionState(updatePassword, undefined);
+  const [state, action, pending] = useActionState(async function (
+    prevState: unknown,
+    formData: FormData
+  ) {
+    const result = await updatePassword(prevState, formData);
+
+    if (result?.success) {
+      toast(result.message, {
+        position: 'top-center',
+        description: (
+          <span className="text-foreground">
+            {new Date().toLocaleString('en-US', {
+              hour12: true,
+              month: 'long',
+              day: '2-digit',
+              weekday: 'long',
+              year: 'numeric',
+              hour: 'numeric',
+              minute: '2-digit'
+            })}
+          </span>
+        )
+      });
+    }
+
+    if (!result?.success && result?.message) {
+      toast(<h2 className="text-destructive">{result?.message}</h2>, {
+        position: 'top-center',
+        description: (
+          <p className="text-destructive">
+            {new Date().toLocaleString('en-US', {
+              hour12: true,
+              month: 'long',
+              day: '2-digit',
+              weekday: 'long',
+              year: 'numeric',
+              hour: 'numeric',
+              minute: '2-digit'
+            })}
+          </p>
+        )
+      });
+    }
+
+    return result;
+  }, undefined);
 
   return (
-    <main className="row-start-2 grid grid-rows-1 p-4">
-      <section className="grid place-items-center gap-4 place-self-center">
-        <form
-          action={action}
-          className="min-w-sm space-y-4 rounded-md border border-dashed p-4"
-        >
-          <div className="space-y-2">
-            <Input id="email" name="email" type="hidden" value={email} />
-          </div>
+    <CN.Card className="min-w-sm">
+      <CN.CardHeader>
+        <CN.CardTitle>Reset your password</CN.CardTitle>
+        <CN.CardDescription>
+          Enter a new password below to reset your account&apos;s password
+        </CN.CardDescription>
+      </CN.CardHeader>
+      <CN.CardContent>
+        <form id="reset-form" className="space-y-2">
+          <Input id="email" name="email" type="hidden" value={email} />
           <div className="space-y-2">
             <Label htmlFor="password">Password</Label>
             <Input
@@ -35,18 +83,19 @@ export default function Component({ email }: { email: string }) {
               </p>
             )}
           </div>
-          {state?.message && (
-            <p className="text-destructive text-xs">{state.message}</p>
-          )}
-          <Button
-            type="submit"
-            disabled={pending}
-            className="w-full cursor-pointer"
-          >
-            {!pending ? 'Update' : 'Updaing Password...'}
-          </Button>
         </form>
-      </section>
-    </main>
+      </CN.CardContent>
+      <CN.CardFooter>
+        <Button
+          type="submit"
+          form="reset-form"
+          disabled={pending}
+          formAction={action}
+          className="w-full cursor-pointer"
+        >
+          {pending ? 'Updating password...' : 'Reset password'}
+        </Button>
+      </CN.CardFooter>
+    </CN.Card>
   );
 }
--- app/create-password/page.tsx
+++ app/create-password/page.tsx
@@ -1,30 +1,40 @@
 import Link from 'next/link';
 import { Suspense } from 'react';
 
+import * as CN from '@/components/ui/card';
 import { verifyToken } from '@/lib/actions';
 import { Button } from '@/components/ui/button';
 import Component from '@/app/create-password/component';
 
 type Props = { searchParams: Promise<{ [key: string]: string | undefined }> };
 
+function ErrorCard({ message }: { message: string }) {
+  return (
+    <CN.Card className="min-w-sm">
+      <CN.CardHeader>
+        <CN.CardTitle>{message}</CN.CardTitle>
+        <CN.CardDescription>
+          Place your token in the url to verify your authenticity
+        </CN.CardDescription>
+      </CN.CardHeader>
+      <CN.CardFooter>
+        <Button className="w-full">
+          <Link href="/">Home</Link>
+        </Button>
+      </CN.CardFooter>
+    </CN.Card>
+  );
+}
+
 async function Verify({ token }: { token: string }) {
   const { error, email, success } = await verifyToken(token);
 
   return (
     <main className="row-start-2 grid place-items-center p-4">
-      {error && (
-        <section className="grid place-items-center gap-2">
-          <p className="text-destructive font-semibold">{error}</p>
-          <Button>
-            <Link href="/">Home</Link>
-          </Button>
-        </section>
-      )}
-      {success && (
-        <section>
-          <Component email={email} />
-        </section>
-      )}
+      <section className="grid place-items-center gap-4 place-self-center">
+        {error && <ErrorCard message={error} />}
+        {success && <Component email={email} />}
+      </section>
     </main>
   );
 }
@@ -36,12 +46,7 @@ export default async function Page({ searchParams }: Props) {
     return (
       <main className="row-start-2 grid place-items-center p-4">
         <section className="grid place-items-center gap-2">
-          <p className="text-destructive font-semibold">
-            Missing verification token!
-          </p>
-          <Button>
-            <Link href="/">Home</Link>
-          </Button>
+          <ErrorCard message="Missing verification token" />
         </section>
       </main>
     );

c5c25d743b2cd35bd19ce4383a792a2d54d37034
Arsalan Ansari <arsalanansariofficial@outlook.com>
2025-07-11 10:13:20 +0530

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,15 @@ import { clsx, type ClassValue } from 'clsx';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getDate() {
+  return new Date().toLocaleString('en-US', {
+    hour12: true,
+    month: 'long',
+    day: '2-digit',
+    weekday: 'long',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: '2-digit'
+  });
+}

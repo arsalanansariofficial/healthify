81e1a015afc0dd74f48b4e18d24a369633b82e03
Arsalan Ansari <arsalanansariofficial@outlook.com>
2025-07-14 13:17:14 +0530

    Update sidebar to have RBAC url list

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,33 +1,38 @@
 'use client';
 
 import Link from 'next/link';
+import { User } from '@/lib/types';
 
 import * as CN from '@/components/ui/card';
+import { hasPermission } from '@/lib/utils';
 import { ScrollArea } from '@/components/ui/scroll-area';
 
 const items = [
-  { url: '/dashboard', label: 'Dashboard' },
-  { url: '/doctors', label: 'Doctors' },
-  { url: '/specialities', label: 'Specialities' }
+  { url: '/dashboard', label: 'Dashboard', permission: 'view:dashboard' },
+  { url: '/users', label: 'Users', permission: 'view:users' },
+  { url: '/doctors', label: 'Doctors', permission: 'view:doctors' }
 ];
 
-export default function Sidebar() {
+export default function Sidebar({ user }: { user: User }) {
   return (
     <aside className="sticky top-[7.35em] hidden max-h-[calc(100vh-10.5em)] min-w-[10em] scroll-mt-0 lg:block">
       <CN.Card className="h-full">
         <ScrollArea className="h-full">
           <CN.CardContent>
             <ul className="space-y-1">
-              {items.map((item, index) => (
-                <li key={index}>
-                  <Link
-                    href={item.url}
-                    className="text-muted-foreground block text-xs font-semibold"
-                  >
-                    {item.label}
-                  </Link>
-                </li>
-              ))}
+              {items.map(
+                (item, index) =>
+                  hasPermission(user.roles, item.permission) && (
+                    <li key={index}>
+                      <Link
+                        href={item.url}
+                        className="text-muted-foreground block text-xs font-semibold"
+                      >
+                        {item.label}
+                      </Link>
+                    </li>
+                  )
+              )}
             </ul>
           </CN.CardContent>
         </ScrollArea>

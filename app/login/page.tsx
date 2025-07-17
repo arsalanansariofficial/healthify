71316fbfb491559dadf293c09ba664394b87741a
Arsalan Ansari <arsalanansariofficial@outlook.com>
2025-07-17 11:56:23 +0530

    Update login page to use display-toast component

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,12 @@
 'use client';
 
-import z from 'zod';
 import Link from 'next/link';
-import { toast } from 'sonner';
 import { signIn } from 'next-auth/react';
 import { useForm } from 'react-hook-form';
 import { useSearchParams } from 'next/navigation';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 import { login } from '@/lib/actions';
-import { getDate } from '@/lib/utils';
 import { SIGNUP } from '@/lib/constants';
 import * as CN from '@/components/ui/card';
 import { loginSchema } from '@/lib/schemas';
@@ -17,49 +14,20 @@ import * as RHF from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import useHookForm from '@/hooks/use-hook-form';
-
-type Schema = z.infer<typeof loginSchema>;
+import handler from '@/components/display-toast';
 
 export default function Page() {
   let oAuthError: string | undefined;
   const searchParams = useSearchParams();
+  const { pending, handleSubmit } = useHookForm(handler, login, oAuthError);
 
-  const { pending, handleSubmit } = useHookForm<Schema>(action);
-  const form = useForm<Schema>({
+  const form = useForm({
     resolver: zodResolver(loginSchema),
-    defaultValues: {
-      email: String(),
-      password: String()
-    }
+    defaultValues: { email: String(), password: String() }
   });
 
   if (searchParams.get('error') === 'OAuthAccountNotLinked') {
     oAuthError = '⚠️ Email already registered with another provider.';
-  }
-
-  async function action(data: Schema) {
-    const result = await login(data);
-
-    if (result?.success) {
-      toast(result.message, {
-        position: 'top-center',
-        description: <span className="text-foreground">{getDate()}</span>
-      });
-    }
-
-    if (oAuthError) {
-      toast(<h2 className="text-destructive">{oAuthError}</h2>, {
-        position: 'top-center',
-        description: <p className="text-destructive">{getDate()}</p>
-      });
-    }
-
-    if (!result?.success && result?.message) {
-      toast(<h2 className="text-destructive">{result?.message}</h2>, {
-        position: 'top-center',
-        description: <p className="text-destructive">{getDate()}</p>
-      });
-    }
   }
 
   return (

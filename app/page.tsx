b5e80c519bbdd731fc5b3e0cedc6d170816d664c
Arsalan Ansari <arsalanansariofficial@outlook.com>
2025-08-14 12:14:13 +0530

    Update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 import Link from 'next/link';
 
 import { LOGIN } from '@/lib/constants';
-import { Button } from '@/components/ui/button';
 
 export default function Page() {
   return (
     <main className="row-start-2 mx-8 grid place-items-center">
       <section className="space-y-4 text-center">
-        <h1 className="text-3xl font-bold">Next-Auth</h1>
-        <Button asChild>
-          <Link href={LOGIN}>Login</Link>
-        </Button>
+        <Link
+          href={LOGIN}
+          className="font-serif text-5xl font-semibold hover:underline hover:underline-offset-16"
+        >
+          Healthify
+        </Link>
       </section>
     </main>
   );

92fd293118ff180ad0919bdf2ca5e0a0a4efeb97
Arsalan Ansari <arsalanansariofficial@outlook.com>
2025-11-11 16:12:33 +0530

    Add component to send account verification email

---
+++ components/email/account/email.tsx
@@ -0,0 +1,11 @@
+import { HOST } from '@/lib/constants';
+
+type Props = { data: { token: string } };
+
+export function VerifyEmail({ data }: Props) {
+  return `<main>
+      <p>
+        Click <a href="${HOST}/verify?token=${data.token}">here</a> to verify your email.
+      </p>
+    </main>`;
+}

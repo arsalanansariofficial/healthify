3c61a6c36eec5d06f7033ce3dfbca42da24d1ee3
Arsalan Ansari <arsalanansariofficial@outlook.com>
2025-11-13 15:40:18 +0530

    Update not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,30 +1,53 @@
-import Link from 'next/link';
+'use client';
 
-import * as CN from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
-import { DASHBOARD, PAGE_NOT_FOUND } from '@/lib/constants';
+import { SearchIcon } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+
+import { Kbd } from '@/components/ui/kbd';
+import { MAIL_TO } from '@/lib/constants';
+import * as EMPTY from '@/components/ui/empty';
+import * as IG from '@/components/ui/input-group';
 
 export default function Page() {
+  const router = useRouter();
+
   return (
     <main className="row-start-2 mx-8 grid place-items-center">
       <section className="space-y-4 text-center">
-        <CN.Card className="min-w-sm">
-          <CN.CardHeader className="text-center">
-            <CN.CardTitle className="content-center font-serif text-3xl font-semibold">
-              😔 404
-            </CN.CardTitle>
-            <CN.CardDescription className="font-serif text-xl font-semibold">
-              {PAGE_NOT_FOUND}
-            </CN.CardDescription>
-          </CN.CardHeader>
-          <CN.CardFooter className="grid gap-2">
-            <Button asChild>
-              <Link href={DASHBOARD} className="font-serif">
-                Home
-              </Link>
-            </Button>
-          </CN.CardFooter>
-        </CN.Card>
+        <EMPTY.Empty>
+          <EMPTY.EmptyHeader>
+            <EMPTY.EmptyTitle>404 - Not Found</EMPTY.EmptyTitle>
+            <EMPTY.EmptyDescription>
+              The page you&apos;re looking for doesn&apos;t exist. Try searching
+              for what you need below.
+            </EMPTY.EmptyDescription>
+          </EMPTY.EmptyHeader>
+          <EMPTY.EmptyContent>
+            <IG.InputGroup className="sm:w-3/4">
+              <form
+                onSubmit={e => {
+                  e.preventDefault();
+                  const formData = new FormData(e.target as HTMLFormElement);
+                  router.push(`/${formData.get('search') as string}`);
+                }}
+              >
+                <IG.InputGroupInput
+                  name="search"
+                  placeholder="Try searching for pages..."
+                />
+              </form>
+              <IG.InputGroupAddon>
+                <SearchIcon />
+              </IG.InputGroupAddon>
+              <IG.InputGroupAddon align="inline-end">
+                <Kbd>/</Kbd>
+              </IG.InputGroupAddon>
+            </IG.InputGroup>
+            <EMPTY.EmptyDescription>
+              Need help? <a href={MAIL_TO}>Contact support</a>
+            </EMPTY.EmptyDescription>
+          </EMPTY.EmptyContent>
+        </EMPTY.Empty>
       </section>
     </main>
   );

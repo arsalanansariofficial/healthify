053af1d4235c12f11e5609d9b8b0e78725855cfd
Arsalan Ansari <arsalanansariofficial@outlook.com>
2025-11-13 15:48:41 +0530

    Update error page to use new empty component

--- app/error.tsx
+++ app/error.tsx
@@ -1,21 +1,56 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
+import { SearchIcon } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+
+import { Kbd } from '@/components/ui/kbd';
+import * as EMPTY from '@/components/ui/empty';
+import * as IG from '@/components/ui/input-group';
+import { IS_PRODUCTION, MAIL_TO } from '@/lib/constants';
 
 type Props = { reset: () => void; error: Error & { digest?: string } };
 
-export default function Error({ error, reset }: Props) {
+export default function Error({ error }: Props) {
+  const router = useRouter();
+
   return (
     <main className="row-start-2 mx-8 grid place-items-center">
       <section className="space-y-4 text-center">
-        <h1 className="text-xl font-bold">{error.message}</h1>
-        <Button
-          variant="secondary"
-          className="cursor-pointer"
-          onClick={() => reset()}
-        >
-          Try again
-        </Button>
+        <EMPTY.Empty>
+          <EMPTY.EmptyHeader>
+            <EMPTY.EmptyTitle>500 - Internal Server Error</EMPTY.EmptyTitle>
+            <EMPTY.EmptyDescription>
+              {!IS_PRODUCTION
+                ? error.message
+                : 'There is some error while processing your request, please try again after some time!'}
+            </EMPTY.EmptyDescription>
+          </EMPTY.EmptyHeader>
+          <EMPTY.EmptyContent>
+            <IG.InputGroup className="sm:w-3/4">
+              <form
+                onSubmit={e => {
+                  e.preventDefault();
+                  const formData = new FormData(e.target as HTMLFormElement);
+                  router.push(`/${formData.get('search') as string}`);
+                }}
+              >
+                <IG.InputGroupInput
+                  name="search"
+                  placeholder="Try searching for pages..."
+                />
+              </form>
+              <IG.InputGroupAddon>
+                <SearchIcon />
+              </IG.InputGroupAddon>
+              <IG.InputGroupAddon align="inline-end">
+                <Kbd>/</Kbd>
+              </IG.InputGroupAddon>
+            </IG.InputGroup>
+            <EMPTY.EmptyDescription>
+              Need help? <a href={MAIL_TO}>Contact support</a>
+            </EMPTY.EmptyDescription>
+          </EMPTY.EmptyContent>
+        </EMPTY.Empty>
       </section>
     </main>
   );

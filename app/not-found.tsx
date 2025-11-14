470be7b5f9e68f0e4de0bd2d258069e80b3a9a2a
Arsalan Ansari <arsalanansariofficial@outlook.com>
2025-11-14 12:30:45 +0530

    Update not-found page to use info component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,53 +1,16 @@
 'use client';
 
-import { SearchIcon } from 'lucide-react';
-import { useRouter } from 'next/navigation';
-
-import { Kbd } from '@/components/ui/kbd';
-import { MAIL_TO } from '@/lib/constants';
-import * as EMPTY from '@/components/ui/empty';
-import * as IG from '@/components/ui/input-group';
+import Info from '@/components/info';
 
 export default function Page() {
-  const router = useRouter();
-
   return (
     <main className="row-start-2 mx-8 grid place-items-center">
       <section className="space-y-4 text-center">
-        <EMPTY.Empty>
-          <EMPTY.EmptyHeader>
-            <EMPTY.EmptyTitle>404 - Not Found</EMPTY.EmptyTitle>
-            <EMPTY.EmptyDescription>
-              The page you&apos;re looking for doesn&apos;t exist. Try searching
-              for what you need below.
-            </EMPTY.EmptyDescription>
-          </EMPTY.EmptyHeader>
-          <EMPTY.EmptyContent>
-            <IG.InputGroup className="sm:w-3/4">
-              <form
-                onSubmit={e => {
-                  e.preventDefault();
-                  const formData = new FormData(e.target as HTMLFormElement);
-                  router.push(`/${formData.get('search') as string}`);
-                }}
-              >
-                <IG.InputGroupInput
-                  name="search"
-                  placeholder="Try searching for pages..."
-                />
-              </form>
-              <IG.InputGroupAddon>
-                <SearchIcon />
-              </IG.InputGroupAddon>
-              <IG.InputGroupAddon align="inline-end">
-                <Kbd>/</Kbd>
-              </IG.InputGroupAddon>
-            </IG.InputGroup>
-            <EMPTY.EmptyDescription>
-              Need help? <a href={MAIL_TO}>Contact support</a>
-            </EMPTY.EmptyDescription>
-          </EMPTY.EmptyContent>
-        </EMPTY.Empty>
+        <Info
+          title="404 - Not Found"
+          message="The page you're looking for doesn't exist. Try searching for
+          what you need below."
+        />
       </section>
     </main>
   );

ed544e261ba6707a808368fd05b1ccb8b025e9fc
Arsalan Ansari <arsalanansariofficial@outlook.com>
2025-11-17 10:56:14 +0530

    Optimize specialites page

--- app/specialities/component.tsx
+++ app/specialities/component.tsx
@@ -2,29 +2,59 @@
 
 import z from 'zod';
 import { toast } from 'sonner';
+import { useMemo } from 'react';
 import { User } from 'next-auth';
 import { useForm } from 'react-hook-form';
-import * as RT from '@tanstack/react-table';
-import * as Icons from '@tabler/icons-react';
+import { ColumnDef } from '@tanstack/react-table';
 import { Speciality, TimeSlot } from '@prisma/client';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { IconDotsVertical } from '@tabler/icons-react';
+import { SPECIALITY_DELETED, SPECIALITIES_DELETED } from '@/lib/constants';
 
-import * as actions from '@/lib/actions';
-import * as CONST from '@/lib/constants';
 import Footer from '@/components/footer';
 import { nameSchema } from '@/lib/schemas';
-import * as RHF from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import useHookForm from '@/hooks/use-hook-form';
 import { User as Doctor } from '@prisma/client';
-import * as Drawer from '@/components/ui/drawer';
 import { useIsMobile } from '@/hooks/use-mobile';
-import * as DT from '@/components/ui/data-table';
 import handler from '@/components/display-toast';
-import * as DM from '@/components/ui/dropdown-menu';
 import { Checkbox } from '@/components/ui/checkbox';
 import { catchErrors, hasPermission } from '@/lib/utils';
+import { DragHandle, DataTable } from '@/components/ui/data-table';
+
+import {
+  deleteSpeciality,
+  updateSpeciality,
+  deleteSpecialities
+} from '@/lib/actions';
+
+import {
+  Form,
+  FormItem,
+  FormField,
+  FormLabel,
+  FormControl,
+  FormMessage
+} from '@/components/ui/form';
+
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+
+import {
+  Drawer,
+  DrawerClose,
+  DrawerTitle,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTrigger,
+  DrawerContent,
+  DrawerDescription
+} from '@/components/ui/drawer';
 
 type TableSchema = { id: number; name: string };
 type TCVProps<T extends z.ZodType> = { item: z.infer<T> };
@@ -43,31 +73,31 @@ type Props = {
 
 function Menu({ id, ids, isHeader = false }: MenuProps) {
   const menuTrigger = (
-    <DM.DropdownMenuTrigger asChild>
+    <DropdownMenuTrigger asChild>
       <Button
         size="icon"
         variant="ghost"
         className="data-[state=open]:bg-muted text-muted-foreground flex size-8"
       >
-        <Icons.IconDotsVertical />
+        <IconDotsVertical />
         <span className="sr-only">Open menu</span>
       </Button>
-    </DM.DropdownMenuTrigger>
+    </DropdownMenuTrigger>
   );
 
   return (
-    <DM.DropdownMenu>
+    <DropdownMenu>
       {!isHeader && menuTrigger}
       {ids && ids.length > 0 && isHeader && menuTrigger}
-      <DM.DropdownMenuContent align="end" className="w-32">
-        <DM.DropdownMenuItem
+      <DropdownMenuContent align="end" className="w-32">
+        <DropdownMenuItem
           variant="destructive"
           onClick={async () => {
             if (!isHeader) {
-              toast.promise(actions.deleteSpeciality(id as string), {
+              toast.promise(deleteSpeciality(id as string), {
                 position: 'top-center',
                 loading: 'Deleting speciality',
-                success: CONST.SPECIALITY_DELETED,
+                success: SPECIALITY_DELETED,
                 error(error) {
                   const { message } = catchErrors(error as Error);
                   return <span className="text-destructive">{message}</span>;
@@ -76,10 +106,10 @@ function Menu({ id, ids, isHeader = false }: MenuProps) {
             }
 
             if (isHeader) {
-              toast.promise(actions.deleteSpecialities(ids as string[]), {
+              toast.promise(deleteSpecialities(ids as string[]), {
                 position: 'top-center',
                 loading: 'Deleting specialities',
-                success: CONST.SPECIALITIES_DELETED,
+                success: SPECIALITIES_DELETED,
                 error(error) {
                   const { message } = catchErrors(error as Error);
                   return <span className="text-destructive">{message}</span>;
@@ -89,9 +119,9 @@ function Menu({ id, ids, isHeader = false }: MenuProps) {
           }}
         >
           Delete
-        </DM.DropdownMenuItem>
-      </DM.DropdownMenuContent>
-    </DM.DropdownMenu>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
 
@@ -100,7 +130,7 @@ export function TableCellViewer<T extends z.ZodType>(props: TCVProps<T>) {
 
   const { pending, handleSubmit } = useHookForm(
     handler,
-    actions.updateSpeciality.bind(null, props.item.id) as (
+    updateSpeciality.bind(null, props.item.id) as (
       data: unknown
     ) => Promise<unknown>,
     true
@@ -112,43 +142,43 @@ export function TableCellViewer<T extends z.ZodType>(props: TCVProps<T>) {
   });
 
   return (
-    <Drawer.Drawer direction={isMobile ? 'bottom' : 'right'}>
-      <Drawer.DrawerTrigger asChild onClick={e => e.currentTarget.blur()}>
+    <Drawer direction={isMobile ? 'bottom' : 'right'}>
+      <DrawerTrigger asChild onClick={e => e.currentTarget.blur()}>
         <Button variant="link" className="text-foreground px-0">
           {props.item.id}
         </Button>
-      </Drawer.DrawerTrigger>
-      <Drawer.DrawerContent>
-        <Drawer.DrawerHeader className="gap-1">
-          <Drawer.DrawerTitle>Speciality</Drawer.DrawerTitle>
-          <Drawer.DrawerDescription>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader className="gap-1">
+          <DrawerTitle>Speciality</DrawerTitle>
+          <DrawerDescription>
             You can change the name for the selected speciality
-          </Drawer.DrawerDescription>
-        </Drawer.DrawerHeader>
+          </DrawerDescription>
+        </DrawerHeader>
         <div className="flex flex-col gap-4 overflow-y-auto px-4 text-sm">
-          <RHF.Form {...form}>
+          <Form {...form}>
             <form
               id="speciality-form"
               className="space-y-2"
               onSubmit={form.handleSubmit(handleSubmit)}
             >
-              <RHF.FormField
+              <FormField
                 name="name"
                 control={form.control}
                 render={({ field }) => (
-                  <RHF.FormItem>
-                    <RHF.FormLabel>Speciality</RHF.FormLabel>
-                    <RHF.FormControl>
+                  <FormItem>
+                    <FormLabel>Speciality</FormLabel>
+                    <FormControl>
                       <Input {...field} type="name" placeholder="Physician" />
-                    </RHF.FormControl>
-                    <RHF.FormMessage />
-                  </RHF.FormItem>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
                 )}
               />
             </form>
-          </RHF.Form>
+          </Form>
         </div>
-        <Drawer.DrawerFooter>
+        <DrawerFooter>
           <Button
             type="submit"
             disabled={pending}
@@ -157,87 +187,95 @@ export function TableCellViewer<T extends z.ZodType>(props: TCVProps<T>) {
           >
             {pending ? 'Saving...' : 'Save'}
           </Button>
-          <Drawer.DrawerClose asChild>
+          <DrawerClose asChild>
             <Button variant="outline">Done</Button>
-          </Drawer.DrawerClose>
-        </Drawer.DrawerFooter>
-      </Drawer.DrawerContent>
-    </Drawer.Drawer>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
   );
 }
 
 export default function Component(props: Props) {
-  const columns: RT.ColumnDef<TableSchema>[] = [
-    {
-      id: 'drag',
-      cell: ({ row }) => <DT.DragHandle id={row.original.id} />
-    },
-    {
-      id: 'select',
-      enableHiding: false,
-      enableSorting: false,
-      header: ({ table }) => (
-        <Checkbox
-          aria-label="Select all"
-          onCheckedChange={value => table.toggleAllPageRowsSelected(!!value)}
-          checked={
-            table.getIsAllPageRowsSelected() ||
-            (table.getIsSomePageRowsSelected() && 'indeterminate')
-          }
-        />
-      ),
-      cell: ({ row }) => (
-        <Checkbox
-          aria-label="Select row"
-          checked={row.getIsSelected()}
-          onCheckedChange={value => row.toggleSelected(!!value)}
-        />
-      )
-    },
-    {
-      id: 'id',
-      header: 'Id',
-      accessorKey: 'id',
-      cell: ({ row }) => (
-        <TableCellViewer
-          key={Date.now()}
-          item={props.specialities.find(s => s.id === String(row.original.id))}
-        />
-      )
-    },
-    {
-      id: 'name',
-      header: 'Name',
-      enableHiding: false,
-      accessorKey: 'name',
-      cell: ({ row }) => (
-        <span>
-          {props.specialities.find(s => s.id === String(row.original.id))?.name}
-        </span>
-      )
-    },
-    {
-      id: 'actions',
-      cell: ({ row }) => (
-        <Menu isHeader={false} id={row.original.id.toString()} />
-      ),
-      header: ({ table }) => {
-        return (
-          <Menu
-            isHeader={true}
-            ids={table
-              .getSelectedRowModel()
-              .rows.map(r => r.original.id.toString())}
+  const columns = useMemo<ColumnDef<TableSchema>[]>(
+    () => [
+      {
+        id: 'drag',
+        cell: ({ row }) => <DragHandle id={row.original.id} />
+      },
+      {
+        id: 'select',
+        enableHiding: false,
+        enableSorting: false,
+        header: ({ table }) => (
+          <Checkbox
+            aria-label="Select all"
+            onCheckedChange={value => table.toggleAllPageRowsSelected(!!value)}
+            checked={
+              table.getIsAllPageRowsSelected() ||
+              (table.getIsSomePageRowsSelected() && 'indeterminate')
+            }
           />
-        );
+        ),
+        cell: ({ row }) => (
+          <Checkbox
+            aria-label="Select row"
+            checked={row.getIsSelected()}
+            onCheckedChange={value => row.toggleSelected(!!value)}
+          />
+        )
+      },
+      {
+        id: 'id',
+        header: 'Id',
+        accessorKey: 'id',
+        cell: ({ row }) => (
+          <TableCellViewer
+            key={Date.now()}
+            item={props.specialities.find(
+              s => s.id === String(row.original.id)
+            )}
+          />
+        )
+      },
+      {
+        id: 'name',
+        header: 'Name',
+        enableHiding: false,
+        accessorKey: 'name',
+        cell: ({ row }) => (
+          <span>
+            {
+              props.specialities.find(s => s.id === String(row.original.id))
+                ?.name
+            }
+          </span>
+        )
+      },
+      {
+        id: 'actions',
+        cell: ({ row }) => (
+          <Menu isHeader={false} id={row.original.id.toString()} />
+        ),
+        header: ({ table }) => {
+          return (
+            <Menu
+              isHeader={true}
+              ids={table
+                .getSelectedRowModel()
+                .rows.map(r => r.original.id.toString())}
+            />
+          );
+        }
       }
-    }
-  ];
+    ],
+    [props.specialities]
+  );
 
   return (
     <div className="flex h-full flex-col gap-8 lg:mx-auto lg:w-10/12">
       {hasPermission(props.user.permissions, 'view:specialities') && (
-        <DT.DataTable
+        <DataTable
           columns={columns}
           data={props.specialities}
           filterConfig={[{ id: 'name', placeholder: 'Name' }]}

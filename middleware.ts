107c631455c5d944c32c8ad9a3d68ca74398ff92
Arsalan Ansari <arsalanansariofficial@outlook.com>
2025-11-18 12:26:32 +0530

    Optimize imports for middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,16 @@ import NextAuth from 'next-auth';
 import { cookies } from 'next/headers';
 import { Permission } from '@prisma/client';
 
-import * as CONST from '@/lib/constants';
 import { auth, authConfig } from '@/auth';
 import { hasPermission } from '@/lib/utils';
+
+import {
+  URLS,
+  LOGIN,
+  SESSION,
+  DASHBOARD,
+  PUBLIC_ROUTES
+} from '@/lib/constants';
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|.*\\.(?:png)$).*)']
@@ -18,30 +25,30 @@ export default NextAuth(authConfig).auth(async request => {
   const isLoggedin = request.auth?.user;
   const path = request.nextUrl.pathname;
 
-  const isAvailableRoute = CONST.URLS.some(u => u.value === path);
-  const isPublicRoute = CONST.PUBLIC_ROUTES.includes(request.nextUrl.pathname);
+  const isAvailableRoute = URLS.some(u => u.value === path);
+  const isPublicRoute = PUBLIC_ROUTES.includes(request.nextUrl.pathname);
   const isExpired = user?.expiresAt && user.expiresAt - Date.now() <= 0;
 
-  const foundUrl = CONST.URLS.find(u => u.value === path);
+  const foundUrl = URLS.find(u => u.value === path);
   const permissions = user?.permissions as Permission[];
   const permission = foundUrl?.permission || String();
 
   const hasUrlPermission = user && hasPermission(permissions, permission);
 
   if (user && isExpired) {
-    sessionCookie.delete(CONST.SESSION);
-    return Response.redirect(new URL(CONST.LOGIN, request.nextUrl));
+    sessionCookie.delete(SESSION);
+    return Response.redirect(new URL(LOGIN, request.nextUrl));
   }
 
   if (user && !isPublicRoute && isAvailableRoute && !hasUrlPermission) {
-    return Response.redirect(new URL(CONST.DASHBOARD, request.nextUrl));
+    return Response.redirect(new URL(DASHBOARD, request.nextUrl));
   }
 
   if (!isPublicRoute && !isLoggedin) {
-    return Response.redirect(new URL(CONST.LOGIN, request.nextUrl));
+    return Response.redirect(new URL(LOGIN, request.nextUrl));
   }
 
   if (isPublicRoute && isLoggedin) {
-    return Response.redirect(new URL(CONST.DASHBOARD, request.nextUrl));
+    return Response.redirect(new URL(DASHBOARD, request.nextUrl));
   }
 });
